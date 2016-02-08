2d5f169fbaff5027881ed1325f2eca288aa9ecfa
Jason Davis <jbdavis80@gmail.com>
2016-02-08 14:11:45 -0500

    logging

--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -9,9 +9,12 @@
     CDVPluginResult* pluginResult = nil;
 
     BOOL canOpenSettings = (&UIApplicationOpenSettingsURLString != NULL);
+    NSLog(@"canOpenSettings:  @", canOpenSettings);
     if (canOpenSettings) {
         NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+        NSLog(@"nsUrl:  @", url);
         [[UIApplication sharedApplication] openURL:url];
+        NSLog(@"opened url");
     }
 
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
