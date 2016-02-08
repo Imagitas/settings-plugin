c19d27825c6fac3b7b91ebd5af10eae25efaab76
Jason Davis <jbdavis80@gmail.com>
2016-02-08 13:54:54 -0500

    remove fruitless conditional

--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -8,12 +8,8 @@
 
     CDVPluginResult* pluginResult = nil;
 
-    if (&UIApplicationOpenSettingsURLString == NULL) {
-      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Settings plugin only available in iOS 8 or greater."];
-    } else {
-      [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
-      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];  
-    }
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
